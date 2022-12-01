c791b645cad35360b5e7832e2476b744c6057acd
RuiMFMoniz <107137952+RuiMFMoniz@users.noreply.github.com>
2022-12-01 13:40:54 +0000

    Update player.cpp
    Tratamento de inputs e erros relacionados

--- player.cpp
+++ player.cpp
@@ -1,5 +1,6 @@
 #include <arpa/inet.h>
 #include <cstring>
+#include <ctype.h>
 #include <errno.h>
 #include <iostream>
 #include <netdb.h>
@@ -17,14 +18,12 @@ using namespace std;
 
 // GLOBAL VARIABLES
 string GSIP = "127.0.0.1";
-string GSPORT = "59034";
+ 
+string GSPORT = "58034";
+ 
+
 
 void flag_decoder(int argc, char *argv[]) {
-
-  cout << argv[1] << endl;
-  cout << argv[2] << endl;
-  // cout << argv[3] << endl;
-  // cout << argv[4] << endl;
 
   if (argc == 1) {
     return;
@@ -35,44 +34,96 @@ void flag_decoder(int argc, char *argv[]) {
       } else if (string(argv[i]) == "-p") {
         GSPORT = argv[i + 1];
       } else {
-        perror("bad input");
+	cerr << "ERROR: unknown flag: " << argv[i] << "\n";
         exit(EXIT_FAILURE);
       }
       i++;
     }
   } else {
+    cerr << "ERROR: invalid application start command\n";
     exit(EXIT_FAILURE);
   }
 
   return;
 }
 
+
 int main(int argc, char *argv[]) {
 
   flag_decoder(argc, argv);
 
-  cout << "GSIP: " << GSIP << " GSPORT: " << GSPORT << endl;
-
+  string command_line;
   string command;
 
   while (1) {
-    std::cin >> command;
+    getline(cin,command_line,'\n');
 
-    if (cin.fail()) {
-      exit(EXIT_FAILURE);
+    string* fields = new string[2];
+    int f_counter = 0;
+    string word;
+    stringstream ss(command_line);
+    while (ss >> word){
+	    if (f_counter > 1){
+		    cerr << "ERROR: Too many command fields. Check the proper command format\n";
+		    exit(EXIT_FAILURE);
+	    }
+	    fields[f_counter] = word;
+	    f_counter++;
     }
 
+
+    command = fields[0];
+
     if (command == "start" || command == "sg") {
-      cout << "here yesss" << endl;
+	    if (fields[1]==""){
+		    cerr << "ERROR: No PLID was given\n";
+		    exit(EXIT_FAILURE);
+	    }
+	    string PLID = fields[1];
     } else if (command == "play" || command == "pl") {
+	    if (fields[1]==""){
+                    cerr << "ERROR: No letter was given\n";
+                    exit(EXIT_FAILURE);
+            }
+            if ((int) fields[1].size() > 1 || isalpha(fields[1][0])==0){
+		    cerr << "ERROR: Input given is not a letter\n";
+		    exit(EXIT_FAILURE);
+	    }
+            char letter = fields[1][0];
     } else if (command == "guess" || command == "gw") {
+	    if (fields[1]==""){
+		    cerr << "ERROR: No guess word was given\n";
+		    exit(EXIT_FAILURE);
+	    }
+	    string guess = fields[1];
     } else if (command == "scoreboard" || command == "sb") {
+	    if (fields[1]!=""){
+		    cerr << "ERROR: Argument not required in this command\n";
+		    exit(EXIT_FAILURE);
+	    }
     } else if (command == "hint" || command == "h") {
+	    if (fields[1]!=""){
+		    cerr << "ERROR: Argument not required in this command\n";
+		    exit(EXIT_FAILURE);
+	    }
     } else if (command == "state" || command == "st") {
+	    if (fields[1]!=""){
+		    cerr << "ERROR: Argument not required in this command\n";
+		    exit(EXIT_FAILURE);
+	    }
     } else if (command == "quit") {
+	    if (fields[1]!=""){
+		    cerr << "ERROR: Argument not required in this command\n";
+		    exit(EXIT_FAILURE);
+	    }
     } else if (command == "exit") {
+	    if (fields[1]!=""){
+		    cerr << "ERROR: Argument not required in this command\n";
+		    exit(EXIT_FAILURE);
+	    }
       exit(EXIT_SUCCESS);
     } else {
+      cerr << "ERROR: Command name not known\n";
       exit(EXIT_FAILURE);
     }
   }
