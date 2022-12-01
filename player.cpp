1b760d547d42e4b609ee8ad2aefe6fc5b595cf3f
RuiMFMoniz <107137952+RuiMFMoniz@users.noreply.github.com>
2022-12-01 22:29:38 +0000

    Update player.cpp
    Start, quit e exit completamente implementados.
    Play apenas implementada a função em caso de resposta certa (letra presente na palavra)

--- player.cpp
+++ player.cpp
@@ -48,6 +48,57 @@ void flag_decoder(int argc, char *argv[]) {
 }
 
 
+string UDP_send_receive(string message){
+	int fd,errcode;
+	ssize_t n;
+	socklen_t addrlen;
+	struct addrinfo hints,*res;
+	struct sockaddr_in addr;
+	char buffer[128];
+
+	fd = socket(AF_INET,SOCK_DGRAM,0);
+	if (fd==-1){
+		exit(1);
+	}
+	memset(&hints,0,sizeof hints);
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_DGRAM;
+
+	errcode = getaddrinfo(GSIP.c_str(),GSPORT.c_str(),&hints,&res);
+	if (errcode!=0){
+		exit(1);
+	}
+
+	n = sendto(fd,message.c_str(),message.size(),0,res->ai_addr,res->ai_addrlen);
+	if (n==-1){
+		exit(1);
+	}
+
+	addrlen= sizeof(addr);
+	n = recvfrom(fd,buffer,128,0,(struct sockaddr*) &addr,&addrlen);
+	if (n==-1){
+		exit(1);
+	}
+
+	write(1,"echo: ",6);
+	write(1,buffer,n);
+
+	freeaddrinfo(res);
+	close(fd);
+	int i=0;
+	string response = "";
+	for(i=0;i<n;i++){
+		response += buffer[i];
+	}
+	return response;
+
+}
+
+
+
+
+
+
 int main(int argc, char *argv[]) {
 
   flag_decoder(argc, argv);
@@ -55,9 +106,13 @@ int main(int argc, char *argv[]) {
   string command_line;
   string command;
 
+  int n_trials = -1;
+  string PLID;
+  int n_letters;
+  char* guessed;
+
   while (1) {
     getline(cin,command_line,'\n');
-
     string* fields = new string[2];
     int f_counter = 0;
     string word;
@@ -79,8 +134,45 @@ int main(int argc, char *argv[]) {
 		    cerr << "ERROR: No PLID was given\n";
 		    exit(EXIT_FAILURE);
 	    }
-	    string PLID = fields[1];
+	    PLID = fields[1];
+	    cout << "PLID:" << PLID << "\n";
+	    string request = "SNG " + fields[1] + "\n";
+	    string response = UDP_send_receive(request);
+	    stringstream rr(response);
+	    rr >> word;
+	    if (word != "RSG"){
+		    cerr << "ERROR: Wrong Protocol Message Received. Terminating connection\n";
+		    exit(EXIT_FAILURE);
+	    }
+	    rr >> word;
+	    string output;
+	    if (word == "OK"){
+		    string n_misses;
+		    rr >> n_letters;
+		    rr >> n_misses;
+		    n_trials = 0;
+		    output = "New game started (max " + n_misses + " errors): ";
+		    guessed = new char[n_letters];  
+		    for(int c = 0; c < n_letters; c++){
+			    output += "_ ";
+			    guessed[c] = '_';
+		    }
+	    }
+	    else if (word == "NOK"){
+		    output = "Game still in progress. Use command 'quit' to end current game";
+	    }
+	    else {
+		    cerr << "ERROR: Wrong Protocol Message Received. Terminating connection\n";
+		    exit(EXIT_FAILURE);
+	    }
+
+	    cout << output + "\n";
+
     } else if (command == "play" || command == "pl") {
+
+	    //FALTA COLOCAR TODAS AS LETRAS RECEBIDAS EM MAISCULAS SEMPRE, PARA NAO HAVER 
+	    //PROBLEMAS DE COERENCIA NO JOGO
+	    //FALTA ACABAR O RESTO DOS STATUS DE RESPOSTA
 	    if (fields[1]==""){
                     cerr << "ERROR: No letter was given\n";
                     exit(EXIT_FAILURE);
@@ -89,7 +181,40 @@ int main(int argc, char *argv[]) {
 		    cerr << "ERROR: Input given is not a letter\n";
 		    exit(EXIT_FAILURE);
 	    }
-            char letter = fields[1][0];
+
+	    n_trials++;
+	    string request = "PLG " + PLID + " " + fields[1] + " " + to_string(n_trials) + "\n";
+	    cout << request;
+	    string response = UDP_send_receive(request);
+	    cout << response;
+	    stringstream rr(response);
+	    rr >> word;
+	    if (word != "RLG"){
+		    cerr << "ERROR: Wrong Protocol Message Received. Terminating connection\n";
+		    exit(EXIT_FAILURE);
+	    }
+	    rr >> word;
+	    if (word == "OK"){
+		    int ocorr;
+		    rr >> word;
+		    rr >> ocorr;
+		    int pos[ocorr];
+		    string output = "";
+		    for (int j=0; j<ocorr; j++){
+			rr >> pos[j];
+		    }
+		    int k=0;
+		    for (int j=0; j<n_letters;j++){
+			if (j==(pos[k]-1)){
+				k++;
+				guessed[j] = fields[1].at(0);
+			}
+			output = output + guessed[j] + " ";
+        	    }
+		    cout << "Yes, \"" + fields[1] +"\" is part of the word: " + output;
+
+	    }		    
+
     } else if (command == "guess" || command == "gw") {
 	    if (fields[1]==""){
 		    cerr << "ERROR: No guess word was given\n";
@@ -116,15 +241,68 @@ int main(int argc, char *argv[]) {
 		    cerr << "ERROR: Argument not required in this command\n";
 		    exit(EXIT_FAILURE);
 	    }
-    } else if (command == "exit") {
+	    if (n_trials != -1){
+	    	string request = "QUT " + PLID + "\n";
+	    	string response = UDP_send_receive(request);
+	    	stringstream rr(response);
+	    	rr >> word;
+            	if (word != "RQT"){
+            		cerr << "ERROR: Wrong Protocol Message Received. Terminating connection\n";    
+	    		exit(EXIT_FAILURE);
+           	}
+	    	rr >> word;
+	    	if (word == "OK"){
+	    		cout << "Ongoing game has been closed\n";
+			n_trials = -1;
+			delete[] guessed;
+	    	}
+	    	else if (word == "ERR"){
+			cout << "No ongoing game has been found\n";
+	    	}
+	    	else{
+			cerr << "ERROR: Wrong Protocol Message Received. Terminating connection\n";
+			exit(EXIT_FAILURE);
+	    	}   
+	    }
+	    else {
+		cerr << "No ongoing game at the moment\n";
+	    }
+            
+    }else if (command == "exit") {
 	    if (fields[1]!=""){
 		    cerr << "ERROR: Argument not required in this command\n";
 		    exit(EXIT_FAILURE);
 	    }
-      exit(EXIT_SUCCESS);
+	    if (n_trials != -1){
+	    	string request = "QUT " + PLID + "\n";
+	    	string response = UDP_send_receive(request);
+	    	stringstream rr(response);
+	    	rr >> word;
+            	if (word != "RQT"){
+            		cerr << "ERROR: Wrong Protocol Message Received. Terminating connection\n";    
+	    		exit(EXIT_FAILURE);
+           	}
+	    	rr >> word;
+	    	if (word == "OK"){
+	    		cout << "Ongoing game has been closed\n";
+			n_trials = -1;
+			delete[] guessed;
+	    	}
+	    	else if (word == "ERR"){
+			cout << "No ongoing game has been found\n";
+	    	}
+	    	else{
+			cerr << "ERROR: Wrong Protocol Message Received. Terminating connection\n";
+			exit(EXIT_FAILURE);
+	    	}   
+	    }
+	    cout << "Exiting player application. Until next time!\n";
+            exit(EXIT_SUCCESS);
     } else {
+      //FAZER ESTE DEFAULT FECHAR A APLICACAO COMO A FUNCAO EXIT, PARA NAO HAVER PROBLEMAS DEPOIS
       cerr << "ERROR: Command name not known\n";
       exit(EXIT_FAILURE);
     }
+    delete[] fields;
   }
 }
